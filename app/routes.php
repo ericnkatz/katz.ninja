45b9aaad53c0f109d4f3e3f3e7bf193087e793d0
Eric Katz <admin@ericnkatz.com>
2014-06-07 23:11:45 -0400

    remove default install routes.

--- app/routes.php
+++ app/routes.php
@@ -10,15 +10,3 @@
 | and give it the Closure to execute when that URI is requested.
 |
 */
-
-Route::group(array('prefix' => 'install'), function()
-{
-	Route::get('/', array('uses' => 'InstallController@start', 'as' => 'wardrobe.install.index'));
-	Route::post('/', array('uses' => 'InstallController@publishAndMigrate', 'as' => 'wardrobe.install.publishAndMigrate'));
-
-	Route::get('user', array('uses' => 'InstallController@createUser', 'as' => 'wardrobe.install.createUser'));
-	Route::post('user', array('uses' => 'InstallController@storeUser', 'as' => 'wardrobe.install.storeUser'));
-
-	Route::get('config', array('uses' => 'InstallController@editConfig', 'as' => 'wardrobe.install.editConfig'));
-	Route::post('config', array('uses' => 'InstallController@updateConfig', 'as' => 'wardrobe.install.updateConfig'));
-});
